dc6bcf4050bdc0ab31ba8b99d97644801210bfb4
BrendaRegis1 <126252761+BrendaRegis1@users.noreply.github.com>
2024-09-24 10:58:32 -0300

    Update question_1.rb

--- questions/question_1.rb
+++ questions/question_1.rb
@@ -14,3 +14,70 @@ três objetos da classe Funcionario e exiba suas informações.
 * Métodos getters e setters.
 * Método para exibir informações.
 =end
+
+#definição da classe
+
+class Funcionario
+    #método initialize é o construtor
+    
+    def initialize(nome, cargo, salario)
+        #atributos privados e encapsulamento
+        @nome = nome
+        @cargo = cargo
+        @salario = salario
+    end
+
+    #getters permitem o acesso aos atributos fora da classe
+    #ex: funcionario1.nome
+    def nome
+        @nome
+    end
+
+    def cargo
+        @cargo
+    end
+
+    def salario
+        @salario
+    end
+    
+
+    #setters permitem a modificação dos atributos fora da classe
+    #ex: funcionario1.nome = "Novo Nome".
+
+    def nome=(nome)
+        @nome = nome
+    end
+
+    def cargo=(cargo)
+        @cargo = cargo
+    end
+
+    def salario=(salario)
+        @salario = salario
+    end
+
+    #metodo que exibe infos do funcionario
+
+    def exibir_infos
+        #acessa os atributos privados
+        #puts imprime cada linha de info na tela
+        
+        puts "Nome: #{@nome}"
+        puts "Cargo: #{@cargo}"
+        puts "Salario: #{@salario}"
+        puts "-----------------------"
+    end
+end
+
+#criação de 3 obj da classe
+#new cria uma nova instancia
+
+funcionario1 = Funcionario.new("Clara Costa", "Gerente", 7000.00)
+funcionario2 = Funcionario.new("Gabriel Mendes", "Desenvolvedor", 5000.00)
+funcionario3 = Funcionario.new("Marina Carvalho", "Analista", 4500.00)
+
+# exibe as infos dos funcionários
+funcionario1.exibir_infos
+funcionario2.exibir_infos
+funcionario3.exibir_infos
