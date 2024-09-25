6dadcca934d258d4f191fa0108382a616b5ed7ed
Bruno D'Luka <brunodlukaa@gmail.com>
2024-09-25 10:51:02 -0300

    feat: Make superhero a sprite character

--- game/base/superhero.rb
+++ game/base/superhero.rb
@@ -1,16 +1,14 @@
 class SuperHero
-  attr_accessor :x, :y, :image, :life, :attack_power, :speed, :last_attack_time, :direction, :direction_timer
+  attr_accessor :x, :y, :image, :life, :attack_power, :speed, :last_attack_time, :direction, :direction_timer, :state
+
+  DEFAULT_SPEED = 3
+  DEFAULT_ATTACK_POWER = 5
+  DEFAULT_HEIGHT = 160
+  DEFAULT_WIDTH = 160
 
   def initialize
-    @image = Image.new('assets/superhero.png', width: 160, height: 160)
-    @x = rand(Window.width - @image.width)
-    @y = GROUND_Y - @image.height
-    @life = 100
-    @attack_power = 5
-    @speed = 3
-    @last_attack_time = Time.now
-    @direction = [:left, :right].sample
-    @direction_timer = rand(60..120)
+    load_animations
+    set_initial_state
     update_position
   end
 
@@ -28,12 +26,14 @@ class SuperHero
 
   def lose_life(amount)
     @life = [@life - amount, 0].max
+    change_state(:hurt)
   end
 
   def attack(target)
     if can_attack? && close_to?(target)
       target.receive_damage(@attack_power)
       @last_attack_time = Time.now
+      change_state(:attack)
     end
   end
 
@@ -51,9 +51,14 @@ class SuperHero
     case @direction
     when :left
       @x = [@x - @speed, 0].max
+      change_state(:walking)
+      flip_sprite_horizontally(true)
     when :right
       @x = [@x + @speed, Window.width - @image.width].min
+      change_state(:walking)
+      flip_sprite_horizontally(false)
     end
+    update_position
   end
 
   def can_attack?
@@ -62,5 +67,64 @@ class SuperHero
 
   def close_to?(target)
     (@x - target.x).abs < 50
+  end
+
+  def load_animations
+    # @idle_image = load_sprite('assets/superhero/superhero_idle.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 300, true)
+    # @attack_image = load_sprite('assets/superhero/superhero_attack.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 100, false)
+    # @hurt_image = load_sprite('assets/superhero/superhero_hurt.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 300, false)
+    # @walk_image = load_sprite('assets/superhero/superhero_walk.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 200, true)
+  
+    @idle_image = load_sprite('assets/hughie/hughie_idle.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 300, true)
+    @attack_image = load_sprite('assets/hughie/hughie_attack.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 100, false)
+    @jump_image = load_sprite('assets/hughie/hughie_jump.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 300, true)
+    @hurt_image = load_sprite('assets/hughie/hughie_hurt.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 300, false)
+    @walk_image = load_sprite('assets/hughie/hughie_walk.png', DEFAULT_WIDTH, DEFAULT_HEIGHT, 200, true)
+  end
+
+  def load_sprite(file, width, height, time, loop)
+    sprite = Sprite.new(file, width: width, height: height, time: time, loop: loop, color: 'black')
+    sprite.remove
+    sprite
+  end
+
+  def set_initial_state
+    @image = @idle_image
+    @x = rand(Window.width - @image.width)
+    @y = GROUND_Y - @image.height
+    @life = 100
+    @attack_power = DEFAULT_ATTACK_POWER
+    @speed = DEFAULT_SPEED
+    @last_attack_time = Time.now
+    @direction = [:left, :right].sample
+    @direction_timer = rand(60..120)
+    @state = :idle
+  end
+
+  def change_state(new_state)
+    return if @state == new_state
+
+    @image.remove
+    @state = new_state
+
+    @image = case @state
+             when :idle then @idle_image
+             when :attack then @attack_image
+             when :hurt then @hurt_image
+             when :walking then @walk_image
+             end
+
+    @image.x = @x
+    @image.y = @y
+    @image.add
+    @image.play { change_state(:idle) } if [:attack, :hurt].include?(@state)
+  end
+
+  def flip_sprite_horizontally(flip)
+    if flip
+      @image.flip_sprite(:horizontal)
+    else
+      @image.flip_sprite(nil)
+    end
   end
 end
