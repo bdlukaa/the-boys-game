182bbbc69c7079b9b6b8973a39396ea441cea445
DanyPLS <137570578+DanyPaes@users.noreply.github.com>
2024-09-27 01:00:14 -0300

    questões 2 e 4

--- questions/question_2.rb
+++ questions/question_2.rb
@@ -1,12 +1,56 @@
 =begin
 ! 2. Sistema de Herança com Animais Conceitos explorados: Herança, superclasse, subclasses.
-Problema: Crie uma superclasse Animal com atributos como nome e idade, 
-e um método emitirson(). Crie subclasses cachorro e Gato que herdam de 
-Animal, e sobrescreva o método emitirson() para que cada subclasse faça 
-o som específico do animal. No método principal, crie objetos das subclasses 
-e faça com que eles emitam o som correspondente.
-
 ! Requisitos:
 * Superclasse Animal com atributos e métodos.
 * Subclasses que herdam de Animal e sobrescrevem métodos.
 =end
+
+#Problema: Crie uma superclasse Animal com atributos como nome e idade, e um método emitirson().
+class Animal   
+    attr_accessor :nome, :idade
+
+    def initialize(nome, idade)
+        @nome = nome
+        @idade = idade
+    end
+
+    def emitirSom()
+    end
+
+end
+
+#Crie subclasses cachorro que herda de Animal
+class Cachorro < Animal
+    
+    def initialize(nome, idade)
+        super(nome, idade)
+    end    
+        
+    def emitirSom() # e sobrescreva o método emitirson(), para que cada subclasse faça o som específico do animal.
+        return "auau"
+    end
+
+end
+
+#Crie subclasse Gato que herda de Animal
+class Gato < Animal
+
+    def initialize(nome, idade)
+        super(nome, idade)
+    end
+    
+    def emitirSom() # e sobrescreva o método emitirson() ...
+        return "miau"
+    end
+
+end
+
+
+
+#No método principal, crie objetos das subclasses e faça com que eles emitam o som correspondente.
+robs = Cachorro.new("Pinduca", 9)
+puts "O #{robs.nome} faz #{robs.emitirSom()}"
+
+
+alfredo = Gato.new("Alfredo", 3)
+puts "E o #{alfredo.nome} faz #{alfredo.emitirSom()}"
--- questions/question_4.rb
+++ questions/question_4.rb
@@ -1,9 +1,70 @@
 =begin
 ! 4. Interface com Meio de Pagamento
 Conceitos explorados: Interfaces, implementação de métodos.
-Problema: Crie uma interface Pagamento com o método realizar Pagamento(). Crie classes Cartaocredito e Boleto que implementam essa interface e definem maneiras diferentes de realizar o pagamento. No método principal, use objetos dessas dasses e faça com que eles realizem o pagamento.
+
 
 ! Requisitos:
 * Interface com método abstrato.
 * Classes que implementam a interface de maneira diferente.
 =end
+
+#Problema: Crie uma interface Pagamento com o método realizar Pagamento().
+class MeiodePagamento
+    def realizarPagamento()
+      raise NotImplementedError, 'Implemente o metódo da classe abstrata'
+    end
+end
+
+
+#contexto
+
+
+
+
+
+#Crie uma classe Boleto que implementa essa interface e define maneiras diferentes de realizar o pagamento. 
+class Boleto < MeiodePagamento
+
+    def initialize(score)
+        @saldo = score * 0.70
+    end
+
+    def realizarPagamento(preco)
+        sleep(10)
+        if preco <= @saldo
+            return "O boleto foi compensado"
+        end
+        return "Não foi possível confirmar o pagamento do boleto"
+    end
+end
+
+#Crie uma classe Cartão que implementa essa interface e define maneiras diferentes de realizar o pagamento. 
+class Cartaocredito < MeiodePagamento
+
+    def initialize(score)
+        @limite = score * 3.5
+    end
+
+    def realizarPagamento(preco)
+        if preco <= @limite
+            return "O pagamento com cartão foi aprovado"
+        end
+        return "Pagamento negado. Limite insuficiente"
+    end
+end
+
+
+
+#No método principal, use objetos dessas dasses e faça com que eles realizem o pagamento.
+
+#cartão
+cartao = Cartaocredito.new 345
+puts "\nCartão:"
+puts cartao.realizarPagamento(5332)
+puts cartao.realizarPagamento(533)
+
+#boleto
+boleto = Boleto.new 765
+puts "\nBoleto:"
+puts boleto.realizarPagamento(999.99)
+puts boleto.realizarPagamento(534)
