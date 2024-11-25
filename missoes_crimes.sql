7d02d5437f864cdfe3a4a26838bd8b93959c0d76
DanyPLS <137570578+DanyPaes@users.noreply.github.com>
2024-11-25 16:28:31 -0300

    schema tabelas e inserts

---
+++ missoes_crimes.sql
@@ -0,0 +1,31 @@
+CREATE TABLE tipos_crimes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE ocorrencias (
+    id SERIAL PRIMARY KEY,                
+    descricao TEXT,               
+    data_crime DATE NOT NULL,              
+    severidade SMALLINT NOT NULL CHECK (severidade BETWEEN 1 AND 10),
+    heroi_id INT NOT NULL,
+    tipo_crime_id INT NOT NULL,
+    CONSTRAINT fk_heroi FOREIGN KEY (heroi_id) REFERENCES heroes(id) ON DELETE SET NULL,
+    CONSTRAINT fk_tipo_crime FOREIGN KEY (tipo_crime_id) REFERENCES tipos_crimes(id) ON DELETE SET NULL
+);
+
+CREATE TABLE missoes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    dificuldade SMALLINT NOT NULL CHECK (dificuldade BETWEEN 1 AND 10),
+    resultado VARCHAR(10) NOT NULL CHECK (resultado IN ('Sucesso', 'Fracasso')),
+    recompensa VARCHAR(50) NOT NULL
+);
+CREATE TABLE missoes_herois (
+    id SERIAL PRIMARY KEY,
+    missao_id INT NOT NULL,
+    heroi_id INT NOT NULL,
+    CONSTRAINT fk_missao FOREIGN KEY (missao_id) REFERENCES missoes(id) ON DELETE SET NULL,
+    CONSTRAINT fk_heroi FOREIGN KEY (heroi_id) REFERENCES heroes(id) ON DELETE SET NULL
+);
