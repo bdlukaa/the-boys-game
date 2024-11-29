1f57ebcb14ba17469a708d7ea30015dd553736d0
DanyPLS <137570578+DanyPaes@users.noreply.github.com>
2024-11-29 17:46:13 -0300

    fix schema
    correções no nome da coluna crime_id
    adição da cláusula unique no nome da missão e na tabela de missoes_herois

--- schema_missoes_crimes.sql
+++ schema_missoes_crimes.sql
@@ -9,14 +9,14 @@ CREATE TABLE ocorrencias (
     data_crime DATE NOT NULL,              
     severidade SMALLINT NOT NULL CHECK (severidade BETWEEN 1 AND 10),
     heroi_id INT NOT NULL,
-    tipo_crime_id INT NOT NULL,
+    crime_id INT NOT NULL,
     CONSTRAINT fk_heroi FOREIGN KEY (heroi_id) REFERENCES heroes(id) ON DELETE SET NULL,
-    CONSTRAINT fk_tipo_crime FOREIGN KEY (tipo_crime_id) REFERENCES tipos_crimes(id) ON DELETE SET NULL
+    CONSTRAINT fk_crime FOREIGN KEY (crime_id) REFERENCES crime(id) ON DELETE SET NULL
 );
 
 CREATE TABLE missoes (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    nome VARCHAR(100) NOT NULL UNIQUE,
     descricao TEXT,
     dificuldade SMALLINT NOT NULL CHECK (dificuldade BETWEEN 1 AND 10),
     resultado VARCHAR(10) NOT NULL CHECK (resultado IN ('Sucesso', 'Fracasso')),
@@ -26,5 +26,6 @@ CREATE TABLE missoes_herois (
     missao_id INT NOT NULL,
     heroi_id INT NOT NULL,
     CONSTRAINT fk_missao FOREIGN KEY (missao_id) REFERENCES missoes(id) ON DELETE SET NULL,
-    CONSTRAINT fk_heroi FOREIGN KEY (heroi_id) REFERENCES heroes(id) ON DELETE SET NULL
+    CONSTRAINT fk_heroi FOREIGN KEY (heroi_id) REFERENCES heroes(id) ON DELETE SET NULL,
+    CONSTRAINT unique_missao_heroi UNIQUE (missao_id, heroi_id)
 );
